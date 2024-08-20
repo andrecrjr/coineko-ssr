d9a9d71d628ab5393b2f053de1e81710047e6d83
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-08-20 19:33:57 -0300

    - removing useEffects and improve caching swr

--- src/components/Hooks/useFetch.tsx
+++ src/components/Hooks/useFetch.tsx
@@ -2,30 +2,11 @@ import { fetchService } from '@/services/ApiService';
 // import { useEffect } from 'react';
 import useSWR from 'swr';
 
-// type Payload = {
-// 	method: 'GET' | 'POST';
-// 	body: string;
-// };
-
 export function useFetch<T>(path: string) {
-	const { data, error, isLoading } = useSWR<T>(path, fetchService.fetchCached);
-
-	// useEffect(() => {
-	// 	const postData = async () => {
-	// 		if (payload?.method === 'POST') {
-	// 			try {
-	// 				const postedData = await fetchService.fetchCached<T>(path, payload);
-	// 				mutate(postedData, false); // Atualiza o cache SWR sem revalidar
-	// 			} catch (error) {
-	// 				throw new Error('Problem with server');
-	// 			}
-	// 		}
-	// 	};
-
-	// 	if (payload.method === 'POST') {
-	// 		postData();
-	// 	}
-	// }, []);
-
+	const { data, error, isLoading } = useSWR<T>(path, fetchService.fetchCached, {
+		revalidateOnFocus: false,
+		dedupingInterval: 900000000,
+		revalidateOnReconnect: false
+	});
 	return { data, error, isLoading };
 }
